0534c71cee4c9ddcd962b301fb93043f82517777
Karolina Woźniczak <85345625+kWozniczak@users.noreply.github.com>
2024-12-14 18:29:21 +0100

    Add flask simple app

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('test')
+main.py       
+from flask import Flask
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Hello world!'
+ 
+@app.route('/hello/<name>')
+ 
+def  hello_name(name):
+    return 'Hello %s!' % name
+ 
+ 
+if __name__ == '__main__':
+    app.run(host ='0.0.0.0', port = 5000, debug = True)
